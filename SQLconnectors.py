e9b7cef786c6f0102b2ab5234bbfe9ec5b4fb33e
WitoldTabaka <80270731+WitoldTabaka@users.noreply.github.com>
2022-07-19 22:36:51 +0200

    Add files via upload

---
+++ SQLconnectors.py
@@ -0,0 +1,75 @@
+import MySQLdb
+import sshtunnel
+import pandas as pd
+
+sshtunnel.SSH_TIMEOUT = 5.0
+sshtunnel.TUNNEL_TIMEOUT = 5.0
+
+def ssh_MySQL_connector_select(userNameSSH,passSSH,ipAddressSSH,portSSH,userNameDB,passDB,dbName,query):
+    # userNameSSH = "string"
+    # passSSH = "string"
+    # ipAddressSSH = "xxx.xxx.x.xxx"
+    # portSSH = int
+    # userNameDB = "string"
+    # passDB = "string#"
+    # dbName = "string"
+    # query = '''SELECT * from table_name'''
+    data = pd.DataFrame()
+
+    with sshtunnel.SSHTunnelForwarder(
+        (ipAddressSSH, portSSH),
+        ssh_username= userNameSSH , ssh_password=passSSH,
+        remote_bind_address=('127.0.0.1', 3306)
+    ) as tunnel:
+        try:
+            connection = MySQLdb.connect(
+                user=userNameDB,
+                passwd=passDB,
+                host='127.0.0.1', port=tunnel.local_bind_port,
+                db= dbName
+            )
+
+            data = pd.read_sql_query(query, connection)
+            #query
+        except BaseException as e:
+            print('problem: ', e)
+            return (data, False)
+
+        connection.close()
+        return (data, True)
+
+
+def ssh_MySQL_connector_insert(userNameSSH,passSSH,ipAddressSSH,portSSH,userNameDB,passDB,dbName,query):
+    # userNameSSH = "string"
+    # passSSH = "string"
+    # ipAddressSSH = "xxx.xxx.x.xxx"
+    # portSSH = int
+    # userNameDB = "string"
+    # passDB = "string#"
+    # dbName = "string"
+    # query = '''INSERT INTO table_name (column1, column2)
+    # VALUES ('value1', 'value1');'''
+    data = pd.DataFrame()
+
+    with sshtunnel.SSHTunnelForwarder(
+        (ipAddressSSH, portSSH),
+        ssh_username= userNameSSH , ssh_password=passSSH,
+        remote_bind_address=('127.0.0.1', 3306)
+    ) as tunnel:
+        try:
+            connection = MySQLdb.connect(
+                user=userNameDB,
+                passwd=passDB,
+                host='127.0.0.1', port=tunnel.local_bind_port,
+                db= dbName
+            )
+            curosr = connection.cursor()
+            curosr.execute(query)
+            connection.commit()
+            #query
+        except BaseException as e:
+            print('problem: ', e)
+            return (False)
+
+        connection.close()
+        return (True)
